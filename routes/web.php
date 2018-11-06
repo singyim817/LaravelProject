5d1dbaff753682cdc1ceb93b8103a6f8793e0778
Sing <sing.yim@nikoyo.com>
2018-11-07 01:41:59 +0800

    Try add routing info

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('/country', 'CountryController@ViewList')->name('Country');
